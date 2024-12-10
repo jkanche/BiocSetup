149ee15fac41ca8d453ce9a6fb296d9b57a62619
jkanche <jkanche@users.noreply.github.com>
2024-12-10 20:41:09 +0000

    Deploying to gh-pages from @ BiocPy/BiocSetup@2575002a52cbdb7ae7507806d5eaf2e9ec3ca462 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.0.post1.dev1+gf9bdd86',
+    VERSION: '0.0.post1.dev1+g2575002',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
